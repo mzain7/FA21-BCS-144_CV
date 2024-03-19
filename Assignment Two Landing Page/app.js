f686f0fb9767392f1e461a35477da14f61174578
mzain7 <mzain.akhtar3@gmail.com>
2024-03-19 14:51:21 +0500

    ajax crud added

--- Assignment Two Landing Page/app.js
+++ Assignment Two Landing Page/app.js
@@ -53,6 +53,79 @@ function submitHandler(event) {
     }
 }
 
+
+function fetchData() {
+    $.ajax({
+        url: 'https://usmanlive.com/wp-json/api/stories',
+        dataType: 'json',
+        success: function (stories) {
+            displayData(stories);
+        },
+        error: function (error) {
+            console.error('Error', error);
+        }
+    });
+}
+
+function displayData(stories) {
+    const listingContainer = $('#listingContainer');
+    listingContainer.empty();
+
+    $.each(stories, function (index, story) {
+        const card = $('<div class="card mb-3"></div>');
+        const cardBody = $('<div class="card-body"></div>');
+
+        cardBody.append('<h5 class="card-title">' + story.title + '</h5>');
+        cardBody.append('<p class="card-text">Content: ' + story.content + '</p>');
+
+        const deleteBtn = $('<button class="btn btn-danger">Delete</button>');
+        deleteBtn.click(function () {
+            deleteData(story.id);
+        });
+
+        cardBody.append(deleteBtn);
+        card.append(cardBody);
+        listingContainer.append(card);
+    });
+}
+
+
+
+function addData(data) {
+    $.ajax({
+        url: 'https://usmanlive.com/wp-json/api/stories',
+        type: 'POST',
+        data: data,
+        success: function () {
+            fetchData();
+            $('#addStoryForm')[0].reset();
+        },
+        error: function (error) {
+            console.error('Error adding story:', error);
+        }
+    });
+}
+
+function deleteData(id) {
+    $.ajax({
+        url: 'https://usmanlive.com/wp-json/api/stories/' + id,
+        type: 'DELETE',
+        success: function () {
+            fetchData(); 
+        },
+        error: function (error) {
+            console.error('Error deleting story:', error);
+        }
+    });
+}
+
+fetchData();
 $(document).ready(function () {
     $("#input-submit").on("click",submitHandler);
+
+    $('#addStoryForm').submit(function (event) {
+        event.preventDefault();
+        addData($(this).serialize());
+    });
 });
+
