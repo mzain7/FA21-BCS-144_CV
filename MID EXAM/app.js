7eb5b04619ee602ab563c6036761bb7685314e8a
mzain7 <mzain.akhtar3@gmail.com>
2024-04-03 13:33:47 +0500

    javascript added

--- MID EXAM/app.js
+++ MID EXAM/app.js
@@ -1,34 +1,19 @@
 const images = document.querySelectorAll("img");
-let ul = document.querySelector('#menu ul');
+let ul = document.querySelector('#menu');
 
 
 
-
-
-document.onload = function () {
+// document.onload = function () {
     images.forEach((img) => {
         console.log(img);
         img.addEventListener('mouseenter', function () {
             let li = document.createElement('li');
+            let a = document.createElement('a');
+            a.href = img.src;
+            a.textContent = img.title;
+            li.appendChild(a);
 
-            li.textContent = img.title;
-
-            ul.prepend(li);
+            ul.appendChild(li);
         });
     });
-};
-
-
-
-// images.forEach((image) => {
-//     image.addEventListener("mouseenter", (e) => {
-//         document.getElementById(
-//             "altText"
-//         ).innerHTML = Image Alt Text: ${ e.target.alt };
-//     });
-//     image.addEventListener("mouseleave", (e) => {
-//         document.getElementById(
-//             "altText"
-//         ).innerHTML = Image Alt Text: Nothing;
-//     });
-// });
+// };
